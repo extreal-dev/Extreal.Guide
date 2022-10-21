976637e3717466b35f0d351902c3b0b63520e37d
kiyohome <kiyohome@users.noreply.github.com>
2022-10-21 07:53:17 +0000

    deploy: bff5dc25b9fc9d279795f71e1bda4b51657ed334

---
+++ assets/js/bdb908fa.f56c077d.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkextreal=self.webpackChunkextreal||[]).push([[395],{44:e=>{e.exports=JSON.parse('{"title":"Core","slug":"/category/core","permalink":"/Extreal.Guide/category/core","navigation":{"previous":{"title":"Unreleased","permalink":"/Extreal.Guide/release/unreleased"},"next":{"title":"Logging","permalink":"/Extreal.Guide/core/logging"}}}')}}]);
