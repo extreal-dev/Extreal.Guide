7b66938554bf8a42d238f686e12795aef526b8d4
sus-taguchi-t <sus-taguchi-t@users.noreply.github.com>
2023-07-24 06:40:14 +0000

    deploy: 260ac4e4917626acdb2c656626ab825277c07e29

---
+++ js/custom.js
@@ -0,0 +1,16 @@
+﻿const VERSION_REGEX = /[0-9]+\.[0-9]+\.[0-9]+/g;
+
+function goTermsOfUse() {
+    let basePath = "/Extreal.Guide/";
+    let path = location.pathname;
+
+    // remove path for release page
+    const releaseIndex = path.indexOf("release");
+    path = releaseIndex >= 0 ? path.substring(0, releaseIndex) : path;
+    
+    basePath = basePath + (path.indexOf("en") >= 0 ? "en/" : "");
+    basePath = basePath + (path.indexOf("next") >= 0 ? "next/" : "");
+    const version = path.match(VERSION_REGEX);
+    basePath = basePath + (version !== null ? `${version[0]}/` : "");
+    location.href = basePath + "legal/terms-of-use";
+}
